ce921cc4b8dd8081331e5f63bbaf598c3eae1e3f
Willian06 <126635903+Willian06@users.noreply.github.com>
2024-08-08 15:08:41 -0300

    /;

--- js/perguntas.js
+++ js/perguntas.js
@@ -98,27 +98,8 @@ export const perguntas = [
             },
         ]
     },
-    {
-        enunciado: "vjdfuvhfdjkbnakvndfkbnjkdsbn",
-        alternativas: [
-            {
-                texto: "uikrytpoginraher.",
-                afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
-                ],
-                proxima: 6,
-            },
-        ]
-    },
+    
+    
     
     
    
